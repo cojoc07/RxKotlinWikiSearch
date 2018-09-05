c29f54559ee424cdab0e152b1dd44f96f212e127
Stefan Cojocaru <cojoc07@gmail.com>
2018-09-05 09:22:34 +0300

    added Room persistence library
    added Room persistence library

--- app/src/main/java/info/stefancojocaru/wikisearch/MainActivity.kt
+++ app/src/main/java/info/stefancojocaru/wikisearch/MainActivity.kt
@@ -2,8 +2,11 @@ package info.stefancojocaru.wikisearch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.text.Html
 import android.widget.Toast
+import info.stefancojocaru.wikisearch.room.DbWorkerThread
+import info.stefancojocaru.wikisearch.room.SearchDatabase
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
@@ -16,6 +19,11 @@ class MainActivity : AppCompatActivity() {
     private var disposable: Disposable? = null
     private var lista: ArrayList<Model.Search>? = null
 
+    private lateinit var mDbWorkerThread: DbWorkerThread
+    private var mDb : SearchDatabase?=null
+    private val mUiHandler = Handler()
+
+
     private val wikiApiServe by lazy {
         WikiApiService.create()
     }
@@ -24,10 +32,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mDbWorkerThread = DbWorkerThread("dbWorkerThread")
+        mDbWorkerThread.start()
+
         btn_search.setOnClickListener {
             if (edit_search.text.toString().isNotEmpty()) {
                 cautare(edit_search.text.toString())
             }
+        }
+        btn_insertdb.setOnClickListener {
+            testInsert()
+        }
+        btn_retrievedb.setOnClickListener {
+            testRetrieve()
         }
     }
 
@@ -67,6 +84,33 @@ class MainActivity : AppCompatActivity() {
                             error -> Toast.makeText(this,error.message,Toast.LENGTH_LONG).show()
                         }
                 )
+    }
+
+    //INSERT INTO ROOM DATABASE
+    private fun testInsert(){
+
+        val search = Model.Search(1,1,"test",1,150,"Room library test","2018-09-12")
+        val task = Runnable{
+            mDb?.searchDao()?.insert(search)
+        }
+        mDbWorkerThread.postTask(task)
+    }
+
+    //RETRIEVE FROM ROOM DATABASE
+    private fun testRetrieve(){
+        val task = Runnable {
+            val weatherData =
+                    mDb?.searchDao()?.getAll()
+            mUiHandler.post({
+                if (weatherData == null || weatherData?.size == 0) {
+
+                    Toast.makeText(this,"No data in cache..!!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this,weatherData?.get(0).toString(),Toast.LENGTH_LONG).show()
+                }
+            })
+        }
+        mDbWorkerThread.postTask(task)
     }
 
     override fun onPause() {
--- app/src/main/java/info/stefancojocaru/wikisearch/Model.kt
+++ app/src/main/java/info/stefancojocaru/wikisearch/Model.kt
@@ -1,9 +1,22 @@
 package info.stefancojocaru.wikisearch
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
 object Model {
     data class Result(val query: Query)
     data class Query(val searchinfo: SearchInfo, val search: Array<Search>)
     data class SearchInfo(val totalhits: Int)
-    data class Search(val ns: Int, val title: String, val pageid: Int, val size: Int, val wordcount: Int,
-                      val snippet: String, val timestamp: String)
+    data class Truck(val roti : Int)
+
+    @Entity(tableName = "search_table")
+    data class Search(
+            @PrimaryKey(autoGenerate = true)
+            val pageid: Int,
+            val ns: Int,
+            val title: String,
+            val size: Int,
+            val wordcount: Int,
+            val snippet: String,
+            val timestamp: String)
 }
